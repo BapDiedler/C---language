d546f08d6575bbbfe19f20586909c6ed936c2e88
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2024-12-08 23:21:40 +0100

    clarification du code

--- src/ast2cfg.ml
+++ src/ast2cfg.ml
@@ -31,6 +31,13 @@ let check_location s data =
                | Some i -> Param i
              end
    | Some i -> Local i
+
+let creat_if data =
+  let l,_ = match data.next with
+            | Label l -> l,data.graph
+            | Block b -> add_block b data.graph
+  in 
+  {data with next = Block ([],(Jmp(l)))}
   
 
 (* cette fonction passe de l'ast vers cfg pour les globales declarations *)
@@ -112,183 +119,124 @@ and transform_code data c =
                   
   | CIF (loc1,(_,loc2),(_,loc3)) -> 
                   let new_r1 = new_reg () in
-                  let l,_ = (match data.next with
-                            | Label l -> l,data.graph
-                            | Block b -> add_block b data.graph
-                              ) 
-                  in 
-                  let data = {data with next = Block ([],(Jmp(l)))} in
-
+                  let data = creat_if data in
                   let data1 = transform_code data loc2 in
-                  let l1, g1 = begin
+                  let l1, g1 =  (
                                 match data1.next with
                                 | Label l-> l, data1.graph
                                 | Block b-> add_block b data.graph
-                              end
-                in
-                let data2 = transform_code {data with graph = g1} loc3 in
-                  let l2, g2 = begin
+                  )in
+                  let data2 = transform_code {data with graph = g1} loc3 in
+                  let l2, g2 =  (
                                 match data2.next with
                                 | Label l-> l, data2.graph
-                                | Block b-> add_block b data1.graph
-                              end
-                  in
+                                | Block b-> add_block b data1.graph          
+                  )in
                   transform_expr new_r1 {data with next = Block ([],JmpC(new_r1, l1, l2)); graph = g2} loc1
 
   | CWHILE (loc_expr,(_,code1)) ->
                 let new_r = new_reg () in
-                let label_prev, graph = (
-                          match data.next with
+                let l,_ = (match data.next with
                           | Label l -> l,data.graph
                           | Block b -> add_block b data.graph
-                  ) in
+                            ) 
+                in 
+                let data = {data with next = Block ([],(Jmp(l)))} in
+
                 let data1 = transform_code data code1 in
-                let label, _ = (
+                let label, g = (
                   match data1.next with
                   | Label l -> l,data1.graph
                   | Block b -> add_block b data1.graph
                 ) in
-                transform_expr new_r {data1 with next = Block ([],JmpC (new_r,label,label_prev))} loc_expr
+
+                
+
+                transform_expr new_r {data1 with next = Block ([],JmpC (new_r,label,l)); graph = g} loc_expr
 
 (* transforme les expressions *)
 and transform_expr r data e =
+
+  let change_data new_nf =
+      match data.next with
+      | Label l-> {data with graph = replace_block l (fun (nf,f) -> (new_nf::nf,f)) data.graph}
+      | Block (nf,f)-> {data with next = Block (new_nf::nf,f)}
+  in
+  
   let (l,ex) = e in
   match ex with
-  | VAR v ->  begin (* appel récursif sur next ? *)
-                match data.next with
-                | Label l-> {data with graph = replace_block l (fun (nf,f) -> (Load (r, check_location v data)::nf,f)) data.graph}
-                | Block (nf,f)-> {data with next = Block (Load (r, check_location v data)::nf,f)}
-              end
+  | VAR v ->  change_data (Load (r, check_location v data))
 
-  | CST v ->  begin (* appel récursif sur next ? Je dois créer un registre pour mettre la constante ou non*)
-                match data.next with
-                | Label l-> Printf.printf "Label %d\n" v;{data with graph = replace_block l (fun (nf,f) -> (Cst (r,v)::nf,f)) data.graph}
-                | Block (nf,f)-> Printf.printf "Block %d\n" v;let l,g = add_block (Cst (r,v)::nf,f) data.graph in {data with next = Label l; graph = g}
-              end
+  | CST v ->  change_data (Cst (r,v))
 
-  | STRING s -> begin (* appel récursif sur next ? *)
-                  match data.next with
-                  | Label l-> {data with graph = replace_block l (fun (nf,f) -> (Cst_string (r,s) ::nf,f)) data.graph}
-                  | Block (nf,f)-> {data with next = Block (Cst_string (r,s) ::nf,f)}
-                end
+  | STRING s -> change_data (Cst_string (r,s))
 
-  | SET_VAR (s,loc) ->  
-                        let new_nf = Store (check_location s data, r) in
-                        let data =  begin (* appel récursif sur next ? *)
-                                      match data.next with
-                                      | Label l-> {data with graph = replace_block l (fun (nf,f) -> (new_nf ::nf,f)) data.graph}
-                                      | Block (nf,f)-> {data with next = Block (new_nf ::nf,f)}
-                                    end 
-                        in
-                        transform_expr (r) data loc (* changer le graphe de data avec res *)
+  | SET_VAR (s,loc) ->  let data =  change_data (Store (check_location s data, r)) in
+                        transform_expr r data loc
 
-  | SET_VAL (s,loc) ->   (* changer le graphe de data avec res *)
-                        let new_nf = StoreI (check_location s data, r) in
-                        let data = 
-                                  begin (* appel récursif sur next ? *)
-                                    match data.next with
-                                    | Label l-> {data with graph = replace_block l (fun (nf,f) -> (new_nf ::nf,f)) data.graph}
-                                    | Block (nf,f)-> {data with next = Block (new_nf ::nf,f)}
-                                  end
-                      in
-                      transform_expr (new_reg ()) data loc
+  | SET_VAL (s,loc) -> let data = change_data (StoreI (check_location s data, r))in
+                        transform_expr (new_reg ()) data loc
 
-  | CALL (s,loc) ->
-                    let new_nf = match VarMap.find_opt s data.local_map with (*check si local*)
-                            | None -> begin match VarMap.find_opt s data.arg_map with (*check si arg*)
-                                      | None -> Call (s) (*check si global*)(*création de registre pour au dessus?*)
-                                      | Some i -> CallR (r)
-                                      end
-                            | Some i -> CallR (r)
+  | CALL (s,loc) -> let new_nf = match VarMap.find_opt s data.local_map with (*check si local*)
+                                | None -> begin match VarMap.find_opt s data.arg_map with (*check si arg*)
+                                          | None -> Call (s) (*check si global*)(*création de registre pour au dessus?*)
+                                          | Some i -> CallR (r)
+                                          end
+                                | Some i -> CallR (r)
                     in
-                    let data =
-                              begin (* appel récursif sur next ? *)
-                                match data.next with
-                                | Label l-> {data with graph = replace_block l (fun (nf,f) -> (new_nf ::nf,f)) data.graph}
-                                | Block (nf,f)-> {data with next = Block (new_nf ::nf,f)}
-                              end
-                    in
+                    let data = change_data new_nf in
                     List.hd (List.map (transform_expr (new_reg ()) data) loc)
 
   | OP1 (op,loc) -> let new_r = new_reg () in
-                    let new_nf =
-                      match op with (* il faut mettre le registre que l'on vient de créer *)
-                      | M_MINUS -> Monop (r,MINUS,new_r)
-                      | M_NOT -> Monop (r,NOT,new_r)
-                      | M_POST_INC -> Monop (r,ADDI 1,new_r)
-                      | M_PRE_INC -> Monop (r,ADDI 1,new_r)
-                      | M_POST_DEC | M_PRE_DEC -> Monop (r,ADDI (-1),new_r)
-                      | M_DEREF -> Monop (r,DEREF,new_r)
-                      | M_ADDR -> Monop (r,MOV,new_r)
+                    let new_nf = match op with (* il faut mettre le registre que l'on vient de créer *)
+                                | M_MINUS -> Monop (r,MINUS,new_r)
+                                | M_NOT -> Monop (r,NOT,new_r)
+                                | M_POST_INC -> Monop (r,ADDI 1,new_r)
+                                | M_PRE_INC -> Monop (r,ADDI 1,new_r)
+                                | M_POST_DEC | M_PRE_DEC -> Monop (r,ADDI (-1),new_r)
+                                | M_DEREF -> Monop (r,DEREF,new_r)
+                                | M_ADDR -> Monop (r,MOV,new_r)
                     in
-                    let data = 
-                              begin (* appel récursif sur next ? *)
-                                match data.next with
-                                | Label l-> {data with graph = replace_block l (fun (nf,f) -> (new_nf ::nf,f)) data.graph}
-                                | Block (nf,f)-> {data with next = Block (new_nf ::nf,f)}
-                              end
-                  in
-                  transform_expr (new_r) data loc
+                    let data = change_data new_nf in
+                    transform_expr (new_r) data loc
 
-  | OP2 (op, loc1, loc2) -> let new_r1 = new_reg () in let new_r2 = new_reg () in
-                            let new_nf = 
-                                    match op with
-                                    | S_MUL -> Binop (r, MUL, new_r1, new_r2)
-                                    | S_DIV -> Binop (r, DIV, new_r1, new_r2)
-                                    | S_MOD -> Binop (r, MOD, new_r1, new_r2)
-                                    | S_ADD -> Binop (r, ADD, new_r1, new_r2)
-                                    | S_SUB -> Binop (r, SUB, new_r1, new_r2)
+  | OP2 (op, loc1, loc2) -> let new_r1 = new_reg () in 
+                            let new_r2 = new_reg () in
+                            let new_nf = match op with
+                                        | S_MUL -> Binop (r, MUL, new_r1, new_r2)
+                                        | S_DIV -> Binop (r, DIV, new_r1, new_r2)
+                                        | S_MOD -> Binop (r, MOD, new_r1, new_r2)
+                                        | S_ADD -> Binop (r, ADD, new_r1, new_r2)
+                                        | S_SUB -> Binop (r, SUB, new_r1, new_r2)
                             in
-                            let data = begin (* appel récursif sur next ? *)
-                              match data.next with
-                              | Label l-> {data with graph = replace_block l (fun (nf,f) -> (new_nf ::nf,f)) data.graph}
-                              | Block (nf,f)-> {data with next = Block (new_nf ::nf,f)}
-                            end in
+                            let data = change_data new_nf in
                             let data = transform_expr (new_r2) data loc2 in
                             transform_expr (new_r1) data loc1
 
   | CMP (cmp, loc1, loc2) ->  let new_r1 = new_reg () in
                               let new_r2 = new_reg () in
-                              let new_nf =
-                                      match cmp with
-                                      | C_LT -> Binop (r, CMP_LT, new_r1, new_r2)
-                                      | C_LE -> Binop (r, CMP_LE, new_r1, new_r2)
-                                      | C_EQ -> Binop (r, CMP_EQ, new_r1, new_r2)
+                              let new_nf = match cmp with
+                                          | C_LT -> Binop (r, CMP_LT, new_r1, new_r2)
+                                          | C_LE -> Binop (r, CMP_LE, new_r1, new_r2)
+                                          | C_EQ -> Binop (r, CMP_EQ, new_r1, new_r2)
                               in
-                              let data = 
-                                        begin (* appel récursif sur next ? *)
-                                          match data.next with
-                                          | Label l-> {data with graph = replace_block l (fun (nf,f) -> (new_nf ::nf,f)) data.graph}
-                                          | Block (nf,f)-> {data with next = Block (new_nf ::nf,f)}
-                                        end
-                              in
+                              let data = change_data new_nf in
                               let data = transform_expr (new_r1) data loc1 in
                               transform_expr (new_r2) data loc2
 
-  | EIF (loc1, loc2, loc3) ->
-                            let l,_ = (match data.next with
-                                      | Label l -> l,data.graph
-                                      | Block b -> add_block b data.graph
-                                        ) 
-                            in 
-                            let data = {data with next = Block ([],(Jmp(l)))} in
+  | EIF (loc1, loc2, loc3) -> let data = creat_if data in
 
                             let data1 = transform_expr r data loc2 in
-                            let l1, g1 = begin
-                              match data1.next with
-                              | Label l-> l, data1.graph
-                              | Block b-> add_block b data1.graph
-                            end
+                            let l1, g1 = match data1.next with
+                                        | Label l-> l, data1.graph
+                                        | Block b-> add_block b data1.graph
                             in
 
                             let data2 = transform_expr r {data with graph = g1} loc3 in
-                            let l2, g2 = begin
-                                  match data2.next with
-                                  | Label l-> l, data2.graph
-                                  | Block b-> add_block b data2.graph
-                                end
+                            let l2, g2 = match data2.next with
+                                        | Label l-> l, data2.graph
+                                        | Block b-> add_block b data2.graph
                             in
-                    
                             transform_expr (r) {data with next = Block ([],JmpC(r, l1, l2)); graph = g2} loc1
 
   | ESEQ ll -> List.fold_right (fun c data -> let new_r = new_reg () in transform_expr new_r data c) ll data
--- src/test.c
+++ src/test.c
@@ -1,8 +1,7 @@
 int c()
 {
   int a;
-  int b;
-  a = 2, b= 5;
-  
-  return a;
+  a = 10;
+  0;
+  return 2;
 }
